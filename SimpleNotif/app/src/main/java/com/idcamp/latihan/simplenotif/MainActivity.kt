95b7e62dc7cd7f9f0e36b70c95379f82a81b50cf
chairul <chairul.amri@ebconnect.com>
2024-02-05 17:13:56 +0700

    [add] Codelab Displays Notifications, Set the display of notifications

--- SimpleNotif/app/src/main/java/com/idcamp/latihan/simplenotif/MainActivity.kt
+++ SimpleNotif/app/src/main/java/com/idcamp/latihan/simplenotif/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.idcamp.latihan.simplenotif
 
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.idcamp.latihan.simplenotif.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = getString(R.string.notification_title)
+        val message = getString(R.string.notification_message)
+
+        binding.btnSendNotification.setOnClickListener {
+            sendNotification(title, message)
+        }
+    }
+
+    private fun sendNotification(title: String, message: String) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setSmallIcon(R.drawable.baseline_notifications_active_24)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setSubText(getString(R.string.notification_subtext))
+
+        val notification = builder.build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 1
+        private const val CHANNEL_ID = "channel_01"
     }
 }
