8e4e2ae25ff7556a590f3d99bd648c890622497b
chairul <chairul.amri@ebconnect.com>
2024-02-05 17:29:22 +0700

    [add] Codelab Displays Notifications, set the PendingIntent action and open the url link in Uri.parse

--- SimpleNotif/app/build.gradle.kts
+++ SimpleNotif/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.idcamp.latihan.simplenotif"
-        minSdk = 24
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
