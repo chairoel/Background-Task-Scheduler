bede33e1d5c68b5138c2ae8ceaa622cd01665a5c
chairul <chairul.amri@ebconnect.com>
2024-02-05 11:05:20 +0700

    [add] BroadcastReceiver Exercises from SMS Events, setup Receiver for Sms Receiver

--- LatihanBroadcastReceiver/app/src/main/java/com/idcamp/latihan/broadcastreceiver/MainActivity.kt
+++ LatihanBroadcastReceiver/app/src/main/java/com/idcamp/latihan/broadcastreceiver/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.idcamp.latihan.broadcastreceiver
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.idcamp.latihan.broadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
+
+    private var requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, "Sms receiver permission diterima", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Sms receiver permission ditolak", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,7 +34,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v?.id) {
-
+            R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
         }
     }
 
