f1c1b72c3aed0c8421a8ffd06d8bc0462076701b
chairul <chairul.amri@ebconnect.com>
2024-02-07 14:19:48 +0700

    [add] Codelab WorkManager, add permissions

--- MyWorkManager/app/src/main/java/com/idcamp/latihan/myworkmanager/MainActivity.kt
+++ MyWorkManager/app/src/main/java/com/idcamp/latihan/myworkmanager/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.idcamp.latihan.myworkmanager
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.work.WorkManager
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
@@ -13,10 +17,25 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var workManager: WorkManager
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Notifications permission granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Notifications permission rejected", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= 33) {
+            requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
 
         workManager = WorkManager.getInstance(this)
         binding.btnOneTimeTask.setOnClickListener(this)
